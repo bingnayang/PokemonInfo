e304e239c91f959a7486aa3be0ad65c7b1da29eb
BingNa Yang <bingnayang@gmail.com>
2021-03-07 15:50:37 -0500

    create pokemon-detail path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 const routes: Routes = [
   {path: 'pokemon-list', component: PokemonListComponent},
+  {path: 'pokemon-detail/:name', component: PokemonDetailComponent},
   {path: '', redirectTo: 'pokemon-list', pathMatch:'full'}
 ];
 
