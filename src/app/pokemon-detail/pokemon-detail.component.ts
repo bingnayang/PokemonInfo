004e9ed2908e9f4cb8ab95b8f26fc8f384356296
BingNa Yang <bingnayang@gmail.com>
2021-03-08 18:23:00 -0500

    get pokemon name

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-detail.component.css']
 })
 export class PokemonDetailComponent implements OnInit {
+  pokemonName: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.pokemonName = this.route.snapshot.params['name'];
+    console.log(this.pokemonName)
   }
 
 }
