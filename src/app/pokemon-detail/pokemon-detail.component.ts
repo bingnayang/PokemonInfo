931cfcea4119538bd45fe8f4e83d02692055a1d7
BingNa Yang <bingnayang@gmail.com>
2021-03-09 18:19:30 -0500

    retrieve pokemon detail from Service

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -8,12 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PokemonDetailComponent implements OnInit {
   pokemonName: string;
+  pokemonDetail: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
     this.pokemonName = this.route.snapshot.params['name'];
     console.log(this.pokemonName)
+    this.getPokemonDetail(this.pokemonName);
+  }
+
+  private getPokemonDetail(name: string){
+    this.pokemonService.getPokemonDetail(name).subscribe(data => {
+      this.pokemonDetail = data;
+      console.log(this.pokemonDetail)
+    })
   }
 
 }
