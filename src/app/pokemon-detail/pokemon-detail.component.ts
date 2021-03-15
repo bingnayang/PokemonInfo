3d5044de9a1aeac9b9113fec0ff535e7d7cbb2cd
BingNa Yang <bingnayang@gmail.com>
2021-03-14 20:54:58 -0400

    update pokemonTypeDetail object and create method to get type detail

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -10,128 +10,133 @@ import { PokemonService } from '../pokemon.service';
 export class PokemonDetailComponent implements OnInit {
   pokemonName: string;
   pokemonDetail: any = {};
+  pokemonType: any[] = [];
+  weakAgainstList : any[] = [];
+  strongAgainstList: any[] = [];
+  resistantToList: any[] = [];
+  vulnerableToList: any[] = [];
   pokemonTypeDetail: any[] = [
     {
-      "type":"Normal",
+      "type":"normal",
       "Strong Against":[],
       "Weak Against":["Rock","Ghost","Steel"],
       "Resistant To":["Ghost"],
       "Vulnerable To":["Fighting"]
     },
     {
-      "type":"Fighting",
+      "type":"fighting",
       "Strong Against":["Normal","Rock","Steel","Ice","Dark"],
       "Weak Against":["Flying","Poison","Psychic","Bug","Ghost","Fairy"],
       "Resistant To":["Rock","Bug","Dark"],
       "Vulnerable To":["Flying","Psychic","Fairy"]
     },
     {
-      "type":"Flying",
+      "type":"flying",
       "Strong Against":["Fighting", "Bug","Grass"],
       "Weak Against":["Rock", "Steel", "Electric"],
       "Resistant To":["Fighting", "Ground", "Bug", "Grass"],
       "Vulnerable To":["Rock", "Electric", "Ice"]
     },
     {
-      "type":"Poison",
+      "type":"poison",
       "Strong Against":["Grass", "Fairy"],
       "Weak Against":["Poison", "Ground", "Rock", "Ghost", "Steel"],
       "Resistant To":["Fighting", "Poison", "Grass", "Fairy"],
       "Vulnerable To":["Ground", "Psychic"]
     },
     {
-      "type":"Ground",
+      "type":"ground",
       "Strong Against":["Poison", "Rock", "Steel", "Fire", "Electric"],
       "Weak Against":["Flying", "Bug", "Grass"],
       "Resistant To":["Poison", "Rock", "Electric"],
       "Vulnerable To":["Water", "Grass", "Ice"]
     },
     {
-      "type":"Rock",
+      "type":"rock",
       "Strong Against":["Flying", "Bug", "Fire", "Ice"],
       "Weak Against":["Fighting", "Ground", "Steel"],
       "Resistant To":["Normal", "Flying", "Poison", "Fire"],
       "Vulnerable To":["Fighting", "Ground", "Steel", "Water", "Grass"]
     },
     {
-      "type":"Bug",
+      "type":"bug",
       "Strong Against":["Grass", "Psychic", "Dark"],
       "Weak Against":["Fighting", "Flying", "Poison", "Ghost", "Steel", "Fire", "Fairy"],
       "Resistant To":["Fighting", "Ground", "Grass"],
       "Vulnerable To":["Flying", "Rock", "Fire"]
     },
     {
-      "type":"Ghost",
+      "type":"ghost",
       "Strong Against":["Ghost", "Psychic"],
       "Weak Against":["Normal", "Dark"],
       "Resistant To":["Normal", "Fighting", "Poison", "Bug" ],
       "Vulnerable To":["Ghost", "Dark"]
     },
     {
-      "type":"Steel",
+      "type":"steel",
       "Strong Against":["Rock", "Ice", "Fairy"],
       "Weak Against":["Steel", "Fire", "Water", "Electric"],
       "Resistant To":["Normal", "Flying", "Poison", "Rock", "Bug", "Steel", "Grass", "Psychic", "Ice", "Dragon", "Fairy"],
       "Vulnerable To":["Fighting", "Ground", "Fire" ]
     },
     {
-      "type":"Fire",
+      "type":"fire",
       "Strong Against":["Bug", "Steel", "Grass", "Ice" ],
       "Weak Against":["Rock", "Fire", "Water", "Dragon"],
       "Resistant To":["Bug", "Steel", "Fire", "Grass", "Ice"],
       "Vulnerable To":["Ground", "Rock", "Water"]
     },
     {
-      "type":"Water",
+      "type":"water",
       "Strong Against":["Ground", "Rock", "Fire"],
       "Weak Against":["Water", "Grass", "Dragon"],
       "Resistant To":["Steel", "Fire", "Water", "Ice"],
       "Vulnerable To":["Grass", "Electric"]
     },
     {
-      "type":"Grass",
+      "type":"grass",
       "Strong Against":["Ground", "Rock", "Water"],
       "Weak Against":["Flying", "Poison", "Bug","Steel", "Fire", "Grass", "Dragon"],
       "Resistant To":["Ground", "Water", "Grass", "Electric"],
       "Vulnerable To":["Flying", "Poison", "Bug", "Fire", "Ice"]
     },
     {
-      "type":"Electric",
+      "type":"electric",
       "Strong Against":["Flying", "Water"],
       "Weak Against":["Ground", "Grass", "Electric", "Dragon"],
       "Resistant To":["Flying", "Steel", "Electric"],
       "Vulnerable To":["Ground"]
     },
     {
-      "type":"Psychic",
+      "type":"psychic",
       "Strong Against":["Fighting", "Poison"],
       "Weak Against":["Steel", "Psychic", "Dark"],
       "Resistant To":["Fighting", "Psychic"],
       "Vulnerable To":["Bug", "Ghost", "Dark"]
     },
     {
-      "type":"Ice",
+      "type":"ice",
       "Strong Against":["Flying", "Ground", "Grass", "Dragon"],
       "Weak Against":["Steel", "Fire", "Water", "Ice"],
       "Resistant To":["Ice"],
       "Vulnerable To":["Fighting", "Rock", "Steel", "Fire"]
     },
     {
-      "type":"Dragon",
+      "type":"dragon",
       "Strong Against":["Dragon"],
       "Weak Against":["Steel", "Fairy"],
       "Resistant To":["Fire", "Water", "Grass", "Electric"],
       "Vulnerable To":["Ice", "Dragon", "Fairy"]
     },
     {
-      "type":"Fairy",
+      "type":"fairy",
       "Strong Against":["Fighting", "Dragon", "Dark"],
       "Weak Against":["Poison", "Steel", "Fire"],
       "Resistant To":["Fighting", "Bug", "Dragon", "Dark"],
       "Vulnerable To":["Poison", "Steel"]
     },
     {
-      "type":"Dark",
+      "type":"dark",
       "Strong Against":["Ghost", "Psychic"],
       "Weak Against":["Fighting", "Dark", "Fairy"],
       "Resistant To":["Ghost", "Psychic", "Dark"],
@@ -145,12 +150,23 @@ export class PokemonDetailComponent implements OnInit {
     this.pokemonName = this.route.snapshot.params['name'];
     console.log(this.pokemonName)
     this.getPokemonDetail(this.pokemonName);
+    this.getPokemonTypeDetail(this.pokemonName);
   }
 
   private getPokemonDetail(name: string){
     this.pokemonService.getPokemonDetail(name).subscribe(data => {
       this.pokemonDetail = data;
-      console.log(this.pokemonDetail)
+    })
+  }
+
+  private getPokemonTypeDetail(name: string){
+    this.pokemonService.getPokemonDetail(name).subscribe(data => {
+      console.log("Pokemon Type:")
+      for(var i of data["types"]){
+        console.log(i["type"]["name"])
+        // console.log(this.pokemonTypeDetail["type"] == i["type"]["name"]);
+        console.log(this.pokemonTypeDetail.find(e => e["type"] === i["type"]["name"]))
+      }
     })
   }
 
