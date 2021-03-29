8afd50ceea69ef93735ca4bdca4bc59d4ab596a8
BingNa Yang <bingnayang@gmail.com>
2021-03-29 17:36:42 -0400

    add pokemon error path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PokemonErrorComponent } from './pokemon-error/pokemon-error.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 const routes: Routes = [
   {path: 'pokemon-list', component: PokemonListComponent},
   {path: 'pokemon-detail/:name', component: PokemonDetailComponent},
+  {path: 'pokemon-error/:name', component: PokemonErrorComponent},
   {path: '', redirectTo: 'pokemon-list', pathMatch:'full'}
 ];
 
