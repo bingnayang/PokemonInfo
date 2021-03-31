4638b2453083ca148fd9bf1c90ebdd60c281c868
BingNa Yang <bingnayang@gmail.com>
2021-03-31 18:12:40 -0400

    add click to button and navigate to home page

--- src/app/pokemon-error/pokemon-error.component.ts
+++ src/app/pokemon-error/pokemon-error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-error',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    this.router.navigate(['pokemon-list']);
+  }
 }
